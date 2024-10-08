53949e34e9957cbfbe0ac21057907221d7cfab90
faelavgtt <rafavergottii@gmail.com>
2024-10-08 10:36:50 -0400

    troca de dataset

--- CienciaDeDados(Bonus)/atividades/Out 08/KaggleDataset/sport.py
+++ CienciaDeDados(Bonus)/atividades/Out 08/KaggleDataset/sport.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("sports_car.csv")
-print(df)
+df = pd.read_csv("https://raw.githubusercontent.com/Faelavgtt/SenacMS/refs/heads/main/CienciaDeDados(Bonus)/atividades/Out%2008/KaggleDataset/dataset/sports_car.csv", header=1)
 
+# print(df.columns)
+# print(df.head()) 
+
+
+print(df.query('"Country of Origin" == "Italy"').head())
+
+#df.to_csv('Country.csv',sep=';',index=False,encoding='utf-8-sig')
