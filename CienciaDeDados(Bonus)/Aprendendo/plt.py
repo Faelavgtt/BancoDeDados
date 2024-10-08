2c3fdf2b0c2804496616114268f436f3dc6de3e9
faelavgtt <rafavergottii@gmail.com>
2024-10-08 07:09:33 -0400

    Update 08/10

---
+++ CienciaDeDados(Bonus)/Aprendendo/plt.py
@@ -0,0 +1,32 @@
+a=(5,2,3,)
+b=(5,2,3,)
+c=(2,3,3,5)
+d=(3,4,2,6)
+# plt.plot(a,b)
+# plt.plot(c,d)
+
+# plt.ylabel(u'altura')
+# plt.xlabel(u'largura')
+# plt.title('Joao')
+# #plt.plot(a,b,"m--")
+# # plt.plot(a,b,"m-*")
+# # plt.plot(c,d,"b-*")
+# plt.axis((0,10,0,20))
+# plt.grid(True)
+
+
+# plt.bar(a,b)
+# plt.scatter(c,d)
+
+
+# explode=(0.1,0,0)
+# labels=["pizza","coca-cola","lasanha"]
+# plt.pie(a,explode=explode,labels=labels,autopct='%.2f%%', shadow=True)
+# plt.title('comida')
+# plt.grid(True)
+
+
+
+
+
+
--- CienciaDeDados(Bonus)/atividades/NBA.py
+++ CienciaDeDados(Bonus)/atividades/NBA.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df=pd.read_csv("all_seasons.csv")
 df1=df.loc[0:11145,["player_height"]]
 df2=df.loc[0:11145,["player_weight"]]
+
+plt.title('NBA')
+plt.scatter(df1,df2)
+
+
+
+plt.show()
