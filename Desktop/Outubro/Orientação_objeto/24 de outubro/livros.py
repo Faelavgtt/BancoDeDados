3cbf2b78c5e8dccab941a42bb7b2bd9d5f66d853
Faelavgtt <rafavergottii@gmail.com>
2024-10-31 11:00:19 -0400

    31 de outubro

---
+++ Desktop/Outubro/Orientação_objeto/24 de outubro/livros.py
@@ -0,0 +1,28 @@
+class Livros():
+    def __init__(self, titulo, autor, genero, cod_livro):
+        self.titulo = titulo
+        self.autor = autor
+        self.genero = genero
+        self.cod_livro = cod_livro
+
+        self.status = "Disponivel"
+        self.usuario = None
+
+    def emprestar_livro(self, usuario):
+        if self.status != 'Disponivel':
+            return
+        
+
+        self.usuario = usuario.nome
+        self.status = 'Emprestado'
+        
+    def devolver_livro(self):
+        if self.status != 'Emprestado':
+            return 'Não pode ser devolvido!'
+        
+        self.usuario = None
+        self.status = 'Disponivel'
+
+
+
+    
